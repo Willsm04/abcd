89d2183aebe43c33ae5851e0d7b9965eb6f1fd5f
Willian Silva <willian.silvameirelea@gmail.com>
2022-08-22 18:37:35 -0300

    nome

---
+++ Beauty_Motos/Pergunta.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Beauty_Motos
+{
+    /// <summary>
+    /// Lógica interna para FrmPerguntaSeDeleta.xaml
+    /// </summary>
+    public partial class FrmPerguntaSeDeleta : Window
+    {
+        public FrmPerguntaSeDeleta()
+        {
+            InitializeComponent();
+        }
+
+        public bool retorno;
+
+        private void btnSim_Click(object sender, RoutedEventArgs e)
+        {
+           retorno = true;
+            Close();
+        }
+
+        private void btnNao_Click(object sender, RoutedEventArgs e)
+        {
+            retorno = false;
+            Close();
+        }
+    }
+}
